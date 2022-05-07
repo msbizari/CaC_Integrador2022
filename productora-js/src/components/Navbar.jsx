9d6b7b6dd1f9485615fc6ffb2f12e2c780061072
msbizari <msbizari@gmail.com>
2022-05-07 20:46:07 -0300

    testing hello world

---
+++ productora-js/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import foto from '../assets/images/jordan-walke.png';
+
+function Navbar(){
+    return(
+        <React.Fragment>
+            <div>
+                <h1 className='prueba'>Hola Mundo!</h1>
+            </div>
+        </React.Fragment>
+    )
+}
+export default Navbar;
