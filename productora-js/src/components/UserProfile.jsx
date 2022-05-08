10b07e08d76c781e1a9538e298a173fe1e36d59b
Martina <martinafsa8>
2022-05-07 21:08:59 -0300

    variables CSS

---
+++ productora-js/src/components/UserProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import foto from '../assets/images/jordan-walke.png';
+
+export default function Profile(){
+    return(
+        <React.Fragment>
+            <div>
+                <h1 className='prueba'>Hola Mundo!</h1>
+            </div>
+        </React.Fragment>
+    )
+}
