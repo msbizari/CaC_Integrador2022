ab3743687ede0db9c1409a9aaa964c5d51f5be9f
Joardev <j.ramos5452@gmail.com>
2022-06-01 00:18:45 -0300

    buscar peliculas usando fetch

--- productora-js/src/App.jsx
+++ productora-js/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <div>
     <React.Fragment>
-      <Navbar />
-      {/* <SearchPage /> */}
+      {/* <Navbar /> */}
+      <SearchPage />
     </React.Fragment>
     </div>
   )
